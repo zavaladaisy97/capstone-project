c54490f673ef08ad07f52db7e7f5946c8fc700a6
daisyalexandria <dazavala7@gmail.com>
2022-08-30 15:14:40 -0500

    added jwt gem and added a sessions routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #users
   post "/users" => "users#create"
+
+  # sessions
+  post "/sessions" => "sessions#create"
+
 
 
 end
